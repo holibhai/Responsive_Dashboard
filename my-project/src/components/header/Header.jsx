bdebd38f0cca4df6303c9ab2c4f068da31daa911
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-10 10:16:38 +0530

    navbar styles

--- my-project/src/components/header/Header.jsx
+++ my-project/src/components/header/Header.jsx
@@ -6,13 +6,13 @@ import {MdSpaceDashboard} from "react-icons/md";
 
 
 
-const Header = () => {
+const Header = ({darkMode}) => {
   return (
     <nav className='fixed top-0 bg-white z-50  w-full border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
         <div className='px-3 py-3 lg:px-5 lg:pl-3'>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center justify-start rtl:justify-end'>
-                   <button>
+                   <button className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
                       <HiOutlineMenuAlt2 className='text-2xl '/>
                    </button>
                    <a href="#" className='flex ms-2 md:me-24'>
@@ -20,6 +20,10 @@ const Header = () => {
                       <span className='self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white'>DaxBod</span>     
                    </a>
                 </div>
+
+                <button className='dark:bg-slate-50 dark:text-slate-700 rounded-full p-2'>
+                   {darkMode?<FaSun/>:<FaMoon/>}
+                </button>
             </div>
         </div>
     </nav>
