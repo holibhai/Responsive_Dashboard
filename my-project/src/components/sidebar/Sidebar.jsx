5a5508771923c54a1c8f6a600d297b351b320401
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-10 11:27:14 +0530

    created sidebar and index.js file

--- my-project/src/components/sidebar/Sidebar.jsx
+++ my-project/src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({isSidebarOpen}) => {
   return (
-    <div>Sidebar</div>
+    <aside className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 transition-transform ${isSidebarOpen?"translate-x-0" :"-translate-x-full"}`}>
+
+      <div className='h-full px-3 pb-4 overflow-y-auto'>
+        <ul className='space-y-2 font-medium'>
+                                
+        </ul>
+      </div>
+    </aside>
   )
 }
 
